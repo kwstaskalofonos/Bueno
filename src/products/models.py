18c6f69478fb1a0531ab796a3bf5f44124ce8bd0
Kwstas kalofonos <konkalof1@gmail.com>
2018-08-03 12:25:16 +0300

    Create Material and Crepe table

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Material(models.Model):
+	SALTY = 'Αλμυρές'
+	SWEET = 'Γλυκές'
+	MATERIAL_CHOICES = (
+		(SALTY,'Αλμυρές'),
+		(SWEET,'Γλυκές')
+	)
+	title = models.CharField(max_length=50)
+	price = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
+	category = models.CharField(max_length=9,choices=MATERIAL_CHOICES,default=SALTY)
+
+	def __str__(self):
+		return self.title
+
+	def __unicode__(self):
+		return self.title
+
+class Crepe(models.Model):
+	SALTY = 'Αλμυρές'
+	SWEET = 'Γλυκές'
+	MATERIAL_CHOICES = (
+		(SALTY,'Αλμυρές'),
+		(SWEET,'Γλυκές')
+	)
+	title = models.CharField(max_length=50)
+	descritpion = models.TextField()
+	price = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+	category = models.CharField(max_length=9,choices=MATERIAL_CHOICES,default=SALTY)
+
+	def __str__(self):
+		return self.title
+
+	def __unicode__(self):
+		return self.title
